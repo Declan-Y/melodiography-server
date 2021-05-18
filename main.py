8cc1a636bfc0a71d118ec08c9def9409e66af376
Declan Young <49220320+Declan-Y@users.noreply.github.com>
2021-05-19 01:06:57 +1000

    remove unused file, remove unused code

--- main.py
+++ main.py
@@ -7,5 +7,4 @@ app = FastAPI()
 @app.get("/generate")
 async def generate_melody():
     melody = midi.generate_random_melody()
-    print(melody.tell())
     return StreamingResponse(melody, media_type="audio/midi", headers={"Content-Disposition": 'attachment; filename="test_file"'})
