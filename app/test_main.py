e1c4ff8de788d9842b1de5bd5e127c04d7d38568
Declan Young <49220320+Declan-Y@users.noreply.github.com>
2021-06-16 00:44:42 +1000

    add content length test to generate endpoint test, add save endpoint test

--- app/test_main.py
+++ app/test_main.py
@@ -8,4 +8,12 @@ client = TestClient(app)
 
 def test_generate():
     response = client.get("/generate")
+    assert len(response.content) > 0
     assert response.status_code == 200
+
+
+def test_save():
+    response = client.post("/save")
+    assert response.status_code == 200
+    
+
