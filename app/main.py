1fd49af17294ffd13d4334ace38c15026f22c9c6
Declan Young <49220320+Declan-Y@users.noreply.github.com>
2021-06-25 23:34:54 +1000

    add test minio server from docs

--- app/main.py
+++ app/main.py
@@ -10,7 +10,8 @@ from minio import Minio
 
 app = FastAPI()
 
-client = Minio("127.0.0.1:9000")
+client = Minio('play.min.io:9000',access_key='Q3AM3UQ867SPQQA43P2F', secret_key='zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG' )
+client.make_bucket("uploads")
 
 def get_db():
     db = SessionLocal()
@@ -35,6 +36,6 @@ async def save_drawing(drawing: schemas.CreateDrawing, db: Session = Depends(get
 
 @app.get("/get-presigned-url")
 async def put_presigned_url():
-    url = client.presigned_get_object("bucket", "object", expires=timedelta(hours=2))
+    url = client.presigned_put_object("uploads", "object", expires=timedelta(hours=2))
     return url
 
