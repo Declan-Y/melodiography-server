a914752f9e8aef490184c43f4ccc01a98e1b53a7
Declan Young <49220320+Declan-Y@users.noreply.github.com>
2021-06-27 00:40:36 +1000

    add tests, fix minio secure flag issue

--- app/test_main.py
+++ app/test_main.py
@@ -9,12 +9,33 @@ client = TestClient(app)
 
 def test_generate():
     response = client.get("/generate")
-    assert len(response.content) > 0
     assert response.status_code == 200
+
+def test_generate_headers():
+    response = client.get("/generate")
+    assert 'content-disposition' in response.headers
+
+def test_generate_content_length():
+    response = client.get("/generate")
+    assert len(response.content) > 0
+
+def test_generate_content_type():
+    response = client.get("/generate")
+    assert response.headers['content-type'] == 'audio/midi'
 
 
 def test_save():
     response = client.post(url="/save", data=json.dumps({'title': 'hello'}))
     assert response.status_code == 200
     
+
+def test_get_presigned_url():
+    response = client.get("/get-presigned-url")
+    assert response.status_code == 200
+
+
+
+def test_put_presigned_url():
+    response = client.get("/put-presigned-url")
+    assert response.status_code == 200
 
