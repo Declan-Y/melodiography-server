cd1a69c89768c1c437d0d9f72db82c79b34dcab5
Declan Young <49220320+Declan-Y@users.noreply.github.com>
2021-07-12 00:31:55 +1000

    fix minio env variables

--- app/object_storage/minio_client.py
+++ app/object_storage/minio_client.py
@@ -12,4 +12,4 @@ MINIO_URL = os.getenv('MINIO_URL')
 SECURE = os.getenv('SECURE')
 
 # client = Minio(MINIO_URL,secure=False, access_key=MINIO_ACCESS_KEY, secret_key=MINIO_SECRET_KEY )
-client = Minio('minio1:9000',secure=False, access_key='admin', secret_key='password' )
+client = Minio(MINIO_URL,secure=False, access_key=MINIO_ACCESS_KEY, secret_key=MINIO_SECRET_KEY )
